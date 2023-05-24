2302af75d171d2e7ca3e5ad151ba0d9a5ece7834
karpekin <karpekin@mail.ru>
2023-05-24 20:05:14 +0300

    task 68

--- Program.cs
+++ Program.cs
@@ -22,15 +22,37 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+// Console.Write("Введите значение M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int summ(int M, int N)
+// {
+// if (M==N)
+//     return N;
+// else 
+//     return M + summ(M + 1, N);
+// }
+// Console.Write(summ(M,N));
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
 Console.Write("Введите значение M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int summ(int M, int N)
+
+int akermann(int M, int N)
 {
-if (M==N)
-    return N;
+if (M == 0) 
+    return N + 1;
+else if (N == 0) 
+    return akermann(M-1, 1);
 else 
-    return M + summ(M + 1, N);
+    return akermann(M-1, akermann(M, N-1));
 }
-Console.Write($"{summ(M,N)}");
+
+Console.Write(akermann(M, N));
