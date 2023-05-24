b5086a38cbf08c2997721712c33505d9a5f2f0ff
karpekin <karpekin@mail.ru>
2023-05-24 19:54:08 +0300

    task 66

--- Program.cs
+++ Program.cs
@@ -3,16 +3,34 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-PrintNumbers (N);
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// PrintNumbers (N);
 
-void PrintNumbers (int N)
+// void PrintNumbers (int N)
+// {
+//     if (N<1)
+//     {
+//         return;
+//     }
+//     Console.Write(N + " ");
+//     PrintNumbers (N-1);
+// }
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int summ(int M, int N)
 {
-    if (N<1)
-    {
-        return;
-    }
-    Console.Write(N + " ");
-    PrintNumbers (N-1);
+if (M==N)
+    return N;
+else 
+    return M + summ(M + 1, N);
 }
+Console.Write($"{summ(M,N)}");
